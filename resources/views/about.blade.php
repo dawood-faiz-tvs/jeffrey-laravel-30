3adfe581e7944fbe674e820c3ef4092f76a80e18
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-04-27 05:32:20 -0700

    day 5
    implemented tailwind ui along with named slots

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:title>
+        About
+    </x-slot:title>
     <h1>Hello from the About Page!</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:title>
+        Home
+    </x-slot:title>
     <h1>Hello from the Home Page!</h1>
 </x-layout>
