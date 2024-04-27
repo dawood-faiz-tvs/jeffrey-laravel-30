266b4a1f78a8f71bd1088c2e2e87390ab6581d4e
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-04-27 05:49:57 -0700

    Revert "day 5"
    This reverts commit 3adfe581e7944fbe674e820c3ef4092f76a80e18.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,3 @@
 <x-layout>
-    <x-slot:title>
-        About
-    </x-slot:title>
     <h1>Hello from the About Page!</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,3 @@
 <x-layout>
-    <x-slot:title>
-        Home
-    </x-slot:title>
     <h1>Hello from the Home Page!</h1>
 </x-layout>
