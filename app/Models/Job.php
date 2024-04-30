b5365626a5833450d7c77b2088071d5207cfa831
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-04-30 05:08:27 -0700

    day 9
    learned about Eloquent and tinker to make jobs dynamr and made jobs dynamic

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,38 +1,7 @@
 <?php
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Junior Developer',
-                'salary' => '$60000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Senior Developer',
-                'salary' => '$80000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Manager',
-                'salary' => '$100000'
-            ]
-        ];
-    }
-
-    public static function find($id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(!$job){
-            abort(404);
-        }
-
-        return $job;
-    }
+class Job extends Model{
+    protected $fillable = ['title','salary'];
 }
-?>
--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/jobs', function () {
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
+    $job = Job::findOrFail($id);
 
     return view('job', [
         'job' => $job
