896eee69d8b3113d471da28398b30e4442690f30
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-05-01 02:33:03 -0700

    day 15
    learned about seedersand how to incorporate factories in them

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'first_name' => 'Dawood',
+            'last_name' => 'Faiz',
+            'email' => 'test@example.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(JobSeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
