caf0eb01f4254443b3b57ba14fbf2d32022a501b
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-05-08 02:55:58 -0700

    day 20
    learned about laravel breeze starter kit for authentication

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -7,59 +7,66 @@ use App\Models\Job;
 
 class JobController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('jobs.index', [
             'jobs' => Job::with('employer')->latest()->simplePaginate(10)
         ]);
     }
 
-    public function store(){
+    public function store()
+    {
         request()->validate([
             'title' => ['required', 'min:3'],
             'salary' => ['required']
         ]);
-    
+
         Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
-    
+
         return redirect('/jobs');
     }
 
-    public function create(){
+    public function create()
+    {
         return view('jobs.create');
     }
 
-    public function update(Job $job){
+    public function update(Job $job)
+    {
         request()->validate([
             'title' => ['required', 'min:3'],
             'salary' => ['required']
         ]);
-    
+
         $job->update([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1
         ]);
-    
+
         return redirect('/jobs/' . $job->id);
     }
 
-    public function destroy(Job $job){
+    public function destroy(Job $job)
+    {
         $job->delete();
 
         return redirect('/jobs');
     }
 
-    public function show(Job $job){
+    public function show(Job $job)
+    {
         return view('jobs.show', [
             'job' => $job
         ]);
     }
 
-    public function edit(Job $job){
+    public function edit(Job $job)
+    {
         return view('jobs.edit', [
             'job' => $job
         ]);
